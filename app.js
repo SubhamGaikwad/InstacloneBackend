f97c93963ee6f8f12e4ce577a4eb5e7115bdc5cd
Subham Gaikwad <subhamgaik358@gmail.com>
2023-01-05 15:17:43 +0530

    created my instaclone backend

---
+++ app.js
@@ -0,0 +1,32 @@
+const express = require("express") ;
+const app = express() ;
+const mongoose = require("mongoose");
+const port = process.env.PORT || 5000
+
+ require('dotenv').config()
+// const dotenv = require("dotenv")
+// dotenv.config({path:"./.env"})
+const cors = require("cors")
+app.use(cors())
+
+mongoose.connect(process.env.MONGO_URL,
+    {useNewUrlParser: true ,
+    useUnifiedTopology: true})
+
+
+
+
+require("./models/user") ;
+require("./models/post") ;
+
+
+app.use(express.json()) ;
+app.use(require("./routes/auth")) ;
+app.use(require("./routes/post")) ;
+app.use(require("./routes/user")) ;
+
+
+
+app.listen(port , () => {
+    console.log("Server is Running " , port);
+})
